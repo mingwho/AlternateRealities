fbe83c4178e413fcd8c95ddd9334ec255e0735c4
mingwho <mh3833@nyu.edu>
2018-02-07 22:37:44 +0400

    uploaded Torus

---
+++ 01_discotunnel/Assets/PipeSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSystem : MonoBehaviour {
+
+    //public Pipe pipePrefab;
+
+    //public int pipeCount;
+
+    //private Pipe[] pipes;
+
+    //private void Awake(){
+    //    pipes = new Pipe[pipeCount];
+    //    for (int i = 0; i < pipes.Length; i++){
+    //        Pipe pipe = pipes[i] = Instantiate<Pipe>(pipePrefab);
+    //        pipe.transform.SetParent(transform,false);
+    //        //if(i > 0){
+    //        //    pipe.AlignWith(pipes[i - 1]);
+    //        //}
+    //    }
+    //}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
