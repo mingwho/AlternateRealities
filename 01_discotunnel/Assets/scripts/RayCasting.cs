7fb2efe6cad3cc45ef9f3fbe26df135e10bcaf40
mingwho <mh3833@nyu.edu>
2018-02-13 01:07:56 +0400

    added raycasting and sphere object

---
+++ 01_discotunnel/Assets/scripts/RayCasting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCasting : MonoBehaviour
+{
+    Camera cam;
+    public GameObject[] players;
+    // Use this for initialization
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit; // raycasthit: a struct that stores the info
+        Ray aRay = cam.ScreenPointToRay(Input.mousePosition);//casting from camera through mouse till infinity
+        if (Physics.Raycast(aRay, out hit))
+        {
+            if (hit.transform.name.Contains("Sphere")){
+                Debug.Log("HELLO WORLD");
+            }
+
+        }
+    }
+}
