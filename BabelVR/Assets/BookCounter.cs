f455aa6e0df4503dc6305584f152d8ae25d2fa67
IM-VR-2 <nyuad.im@nyu.edu>
2018-02-27 02:37:10 -0800

    showing how many books we have grabbed

---
+++ BabelVR/Assets/BookCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class BookCounter : MonoBehaviour {
+
+	VRTK_InteractableObject bookInteraction;
+	private bool Grabbed = false;
+	private bool pGrabbed = false;
+
+	// Use this for initialization
+	void Start () {
+		bookInteraction = this.GetComponent<VRTK_InteractableObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 Grabbed = bookInteraction.IsGrabbed();
+
+		if (pGrabbed == false && Grabbed == true) {
+			BooksGrabbedCount.booksGrabbed++;
+			pGrabbed = true;
+		} 
+
+	}
+}
